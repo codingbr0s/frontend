92d8e9a7249e20717839c5f36192d34f03c4db84
lion <lion.haupt@ibs.valantic.com>
2019-05-30 17:23:58 +0200

    starting to create base structure

---
+++ src/views/App/App.vue
@@ -0,0 +1,12 @@
+<template src="./App.html">
+</template>
+
+<style scoped lang="scss" src="./App.scss">
+</style>
+
+<script lang="ts" src="./App.ts">
+</script>
+
+
+<style scoped lang="scss">
+</style>
