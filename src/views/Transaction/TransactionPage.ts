c7a84ae66579627bfdd0d99807c3b1f02f6aef5b
lion <lion.haupt@ibs.valantic.com>
2019-06-01 15:14:08 +0200

    add binding for transaction form

--- src/views/Transaction/TransactionPage.ts
+++ src/views/Transaction/TransactionPage.ts
@@ -1,10 +1,37 @@
-import {Component, Vue} from 'vue-property-decorator';
-import {Money} from 'v-money';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import {Invoice} from '../../shared/model/invoice';
+import {invoice} from '../../shared/store/modules/invoice';
+import {namespace} from 'vuex-class';
+import {VMoney} from 'v-money';
+
+const InvoiceModule = namespace('invoice');
 
 @Component({
   components: {},
-  directives: {money: Money},
+  directives: {money: VMoney},
 })
 export default class TransactionPage extends Vue {
+  @InvoiceModule.State('invoice') public stateInvoice!: Invoice;
+
+  @Prop(Object)
+  private invoiceInput?: Invoice;
+
+  private money: any = {
+    decimal: ',',
+    thousands: '.',
+    prefix: '',
+    suffix: ' €',
+    precision: 2,
+    masked: false /* doesn't work with directive */};
+
+  get invoice(): Invoice {
+    return this.stateInvoice;
+  }
+
+  private mounted() {
+    if (this.invoiceInput !== undefined && this.invoiceInput !== null) {
+      this.stateInvoice = this.invoiceInput!;
+    }
+  }
 
 }
